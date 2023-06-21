84e302ee0d6025a883160f6e9437ff29fc8f7cd9
geoweb999 <55800507+geoweb999@users.noreply.github.com>
2023-06-21 08:40:05 -0700

    exercise 4

--- js/js exercises/javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ js/js exercises/javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    if (arguments.length < 2) {
+        return "ERROR";
+    }
+    const out = [];
+    const pattern = [];
+    const arr = arguments[0];
+    for (let i=1; i < arguments.length; i++) {
+        pattern.push(arguments[i]);
+    }
+    for (let i=0; i < arr.length; i++) {
+        if (pattern.includes(arr[i])) {
+            continue
+        } else {
+            out.push(arr[i]);
+        }
+    }
+    return out;
 
 };
 

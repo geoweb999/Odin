1d12cbd28b24e0dd48c23f77ca2e23186da34bed
geoweb999 <55800507+geoweb999@users.noreply.github.com>
2023-06-21 08:47:05 -0700

    exercise 5

--- js/js exercises/javascript-exercises-main/05_sumAll/sumAll.js
+++ js/js exercises/javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(p1, p2) {
+    if (typeof p1 != "number" || typeof p2 != "number") {
+        return "ERROR"
+    }
+    if (p1 < 0 || p2 < 0) {
+        return "ERROR"
+    }
+    let sum = 0;
+    if (p1 <= p2) {
+        for (let i = p1; i <= p2; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = p2; i <= p1; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
